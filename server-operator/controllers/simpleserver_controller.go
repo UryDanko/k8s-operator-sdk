f7e427bf57c94ebe0f1a320019473c41aa03d9da
Yuriy Danko <ydanko@ebay.com>
2021-07-16 04:23:20 +0300

    Extract deployment creation

--- server-operator/controllers/simpleserver_controller.go
+++ server-operator/controllers/simpleserver_controller.go
@@ -19,11 +19,7 @@ package controllers
 import (
 	"fmt"
 	appsv1 "k8s.io/api/apps/v1"
-	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/types"
-	"reflect"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"time"
 
@@ -60,7 +56,7 @@ type SimpleServerReconciler struct {
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.8.3/pkg/reconcile
 func (r *SimpleServerReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	log := ctrllog.FromContext(ctx)
-
+	timeout := 60 * time.Second
 	log.Info("Start reconciling loop")
 
 	// Fetch the SimpleServer instance
@@ -79,124 +75,25 @@ func (r *SimpleServerReconciler) Reconcile(ctx context.Context, req ctrl.Request
 		return ctrl.Result{}, err
 	}
 
-	// Check if the deployment already exists, if not create a new one
-	found := &appsv1.Deployment{}
-	err = r.Get(ctx, types.NamespacedName{Name: simpleServer.Name, Namespace: simpleServer.Namespace}, found)
-	if err != nil && errors.IsNotFound(err) {
-		// Define a new deployment
-		dep := r.createDeployment(simpleServer)
-		log.Info("Creating a new Deployment", "Deployment.Namespace", dep.Namespace, "Deployment.Name", dep.Name)
-		err = r.Create(ctx, dep)
-		if err != nil {
-			log.Error(err, "Failed to create new Deployment", "Deployment.Namespace", dep.Namespace, "Deployment.Name", dep.Name)
-			return ctrl.Result{}, err
-		}
-		// Deployment created successfully - return and requeue
-		return ctrl.Result{Requeue: true}, nil
-	} else if err != nil {
-		log.Error(err, "Failed to get Deployment")
-		return ctrl.Result{}, err
-	}
-
-	// Ensure the deployment size is the same as the spec
-	size := simpleServer.Spec.Size
-	if *found.Spec.Replicas != size {
-		found.Spec.Replicas = &size
-		err = r.Update(ctx, found)
-		if err != nil {
-			log.Error(err, "Failed to update Deployment", "Deployment.Namespace", found.Namespace, "Deployment.Name", found.Name)
-			return ctrl.Result{}, err
-		}
-		// Ask to requeue after 1 minute in order to give enough time for the
-		// pods be created on the cluster side and the operand be able
-		// to do the next update step accurately.
-		return ctrl.Result{RequeueAfter: time.Minute}, nil
-	}
-
-	// Update the SimpleServer status with the pod names
-	// List the pods for this SimpleServer's deployment
-	podList := &corev1.PodList{}
-	listOpts := []client.ListOption{
-		client.InNamespace(simpleServer.Namespace),
-		client.MatchingLabels(makeLabels(simpleServer.Name)),
-	}
-	if err = r.List(ctx, podList, listOpts...); err != nil {
-		log.Error(err, "Failed to list pods", "SimpleServer.Namespace", simpleServer.Namespace, "SimpleServer.Name", simpleServer.Name)
-		return ctrl.Result{}, err
-	}
-	podNames := getPodNames(podList.Items)
-
-	// Update status.Nodes if needed
-	if !reflect.DeepEqual(podNames, simpleServer.Status.Nodes) {
-		simpleServer.Status.Nodes = podNames
-		err := r.Status().Update(ctx, simpleServer)
-		if err != nil {
-			log.Error(err, "Failed to update SimpleServer status")
-			return ctrl.Result{}, err
-		}
+	err = r.ReconcileDeployment(ctx, log, simpleServer)
+	if err != nil {
+		log.Error(err, fmt.Sprintf("failed to reconcile Deployment, retrying in %ss", timeout))
+		return ctrl.Result{RequeueAfter: timeout}, err
 	}
 
 	err = r.ReconcileService(ctx, log, simpleServer)
 	if err != nil {
-		timeout := 60 * time.Second
 		log.Error(err, fmt.Sprintf("failed to reconcile Service, retrying in %ss", timeout))
 		return ctrl.Result{RequeueAfter: timeout}, err
 	}
 
 	err = r.ReconcileConfigMap(ctx, log, simpleServer)
 	if err != nil {
-		timeout := 60 * time.Second
 		log.Error(err, fmt.Sprintf("failed to reconcile Service, retrying in %ss", timeout))
 		return ctrl.Result{RequeueAfter: timeout}, err
 	}
 
 	return ctrl.Result{}, nil
-}
-
-// createDeployment returns a SimpleServer Deployment object
-func (r *SimpleServerReconciler) createDeployment(m *sandboxv1alpha1.SimpleServer) *appsv1.Deployment {
-	ls := makeLabels(m.Name)
-	replicas := m.Spec.Size
-
-	dep := &appsv1.Deployment{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      m.Name,
-			Namespace: m.Namespace,
-		},
-		Spec: appsv1.DeploymentSpec{
-			Replicas: &replicas,
-			Selector: &metav1.LabelSelector{
-				MatchLabels: ls,
-			},
-			Template: corev1.PodTemplateSpec{
-				ObjectMeta: metav1.ObjectMeta{
-					Labels: ls,
-				},
-				Spec: corev1.PodSpec{
-					Containers: []corev1.Container{{
-						Image: "ydanko/simple-server:latest",
-						Name:  "simpleserver",
-						Ports: []corev1.ContainerPort{{
-							ContainerPort: 8080,
-							Name:          "simpleserver",
-						}},
-					}},
-				},
-			},
-		},
-	}
-	// Set instance as the owner and controller
-	ctrl.SetControllerReference(m, dep, r.Scheme)
-	return dep
-}
-
-// getPodNames returns the pod names of the array of pods passed in
-func getPodNames(pods []corev1.Pod) []string {
-	var podNames []string
-	for _, pod := range pods {
-		podNames = append(podNames, pod.Name)
-	}
-	return podNames
 }
 
 // SetupWithManager sets up the controller with the Manager.
